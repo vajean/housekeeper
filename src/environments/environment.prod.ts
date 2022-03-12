7d81606df1b20116173055510b12d1fb237e81bd
Vajean <vajeans@gmail.com>
2022-03-13 00:05:11 +0100

    first production run

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBa_3E-ogAgDR5mtrdptN82DdIiCG9DQKQ",
+    authDomain: "housekeeper-2fb9e.firebaseapp.com",
+    projectId: "housekeeper-2fb9e",
+    storageBucket: "housekeeper-2fb9e.appspot.com",
+    messagingSenderId: "818506953225",
+    appId: "1:818506953225:web:44d326ed8d8afef74cbed8"
+  }
 };
