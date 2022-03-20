4a3f0fc979b5ce01fe3ac30f8c9c43fcf086ad49
vajean stan <vajeans@gmail.com>
2022-03-20 20:53:00 +0100

    nav bar routing works

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../core/auth.service';
-import { AngularFireAuth } from "@angular/fire/compat/auth";
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../core/auth.service';
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-profile',
@@ -9,19 +9,39 @@ import { AngularFireAuth } from "@angular/fire/compat/auth";
 })
 export class ProfileComponent implements OnInit {
 
+  home_tab_visibility = 'active';
+  rooms_tab_visibility = '';
+  tasks_tab_visibility = '';
+
   constructor(
     private authservice: AuthService,
-    public afAuth: AngularFireAuth
-  ) { }
+    public afAuth: AngularFireAuth,
+  ) {
+  }
 
   ngOnInit(): void {
   }
+
   logout() {
     this.authservice.logout();
   }
 
+  nav_home() {
+    this.home_tab_visibility = 'active';
+    this.rooms_tab_visibility = '';
+    this.tasks_tab_visibility = '';
+  }
+
+  nav_rooms() {
+    this.home_tab_visibility = '';
+    this.rooms_tab_visibility = 'active';
+    this.tasks_tab_visibility = '';
+  }
+
   nav_tasks() {
-    console.log('bau')
+    this.home_tab_visibility = '';
+    this.rooms_tab_visibility = '';
+    this.tasks_tab_visibility = 'active';
   }
 
 }
